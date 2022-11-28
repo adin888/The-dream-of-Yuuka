432a176e78aaedcc67668a0ee4f1bdb1a9c93d9e
HR94 <1015111463@qq.com>
2022-11-28 14:15:11 +0100

    Input System

---
+++ Assets/Script/Player/Input System/InputHandler.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputHandler : MonoBehaviour
+{
+    public Vector2 RawMovementInput { get; private set; }
+    public int NormInputX { get; private set; }
+    public int NormInputY { get; private set; }
+    public bool JumpInput { get; private set; }
+    public bool JumpInputStop { get; private set; }
+
+    private float jumpInputStartTime;
+
+    public void OnMoveInput(InputAction.CallbackContext context)
+    {
+        RawMovementInput = context.ReadValue<Vector2>();
+        // normalize the input value
+        NormInputX = Mathf.RoundToInt(RawMovementInput.x);
+        NormInputY = Mathf.RoundToInt(RawMovementInput.y);
+    }
+    public void OnJumpInput(InputAction.CallbackContext context)
+    {
+        if(context.started)
+        {
+            JumpInput = true;
+            JumpInputStop = false;
+            jumpInputStartTime = Time.time;
+        }
+        if(context.canceled)
+        {
+            JumpInputStop = true;
+        }
+    }
+
+}
